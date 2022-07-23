de02c1f6ff479a34bcc08bec5408be717e781d15
kevinlau2102 <kevinlaurence49@gmail.com>
2022-07-24 02:02:15 +0700

    update praktikum 4 pemrograman terapan

---
+++ Prak_PT04_2072030/src/main/java/com/example/prak_pt04_2072030/Dao/DaoInterface.java
@@ -0,0 +1,9 @@
+package com.example.prak_pt04_2072030.Dao;
+
+import javafx.collections.ObservableList;
+
+public interface DaoInterface<T> {
+    ObservableList<T> getData();
+    void addData(T data);
+    void deleteData(T data);
+}
